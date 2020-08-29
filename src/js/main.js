93bdff11fdbcde371ac6cb920c7b3227a34cfa08
HuanaMari <marija.kuzmanoska@gmail.com>
2020-08-29 20:50:38 +0200

    css

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,57 @@
 //SCROLL TO NEXT SECTION
-$(".scrolldown").click(function(e) {
-    e.preventDefault();
-    var position = $(this).closest('section').next('section').offset().top;
-    $("body, html").animate({
-      scrollTop: position
-    } ,1100 );
+$(".scrolldown").click(function (e) {
+  e.preventDefault();
+  var position = $(this).closest('section').next('section').offset().top;
+  $("body, html").animate({
+    scrollTop: position
+  }, 1100);
 });
 // HIDE MENU
-$('body').click(function(e){    
-  if(e.target.id == "navbarNav")        return;
+$('body').click(function (e) {
+  if (e.target.id == "navbarNav") return;
   // For descendants of menu_content being clicked, remove this check if you do not want to put constraint on descendants.
-  if($(e.target).closest('#navbarNav').length)        return;
+  if ($(e.target).closest('#navbarNav').length) return;
 
   // Do processing of click event here for every element except with id menu_content
   if ($('#navbarLang').hasClass('show')) {
-      $('.navbar-toggler.lang-toggler')[0].click();
+    $('.navbar-toggler.lang-toggler')[0].click();
   }
 });
 
-//Carousel index//
+//Carousel #solutionSlider//
+const carousel = {
+  carousel: $("#solutionSlider"),
+  carouselItems: $("#solutionSlider .carousel-inner").children(),
+  cssClasses: ['solutionOne', 'solutionTwo', 'solutionThree']
+}
 
-var totalItems = $('.carousel-item').length;
-var currentIndex = $('#solutionSlider.carousel-item.active').index() + 1;
 
-$('#solutionSlider').on('slid.bs.carousel', function() {
-    currentIndex = $('div.active').index() + 1;
-   $('.num').html(currentIndex);
+function carouselPseudoClassHandler() {
+  for (let index = 0; index < carousel.carouselItems.length; index++) {
+    let currentItem = carousel.carouselItems[index];
+    if ($(currentItem).hasClass('active')) {
+      console.log('add',index,carousel.cssClasses[index])
+      carousel.carousel.addClass(carousel.cssClasses[index])
+    } else {
+      carousel.carousel.removeClass(carousel.cssClasses[index])
+    }
+  }
+}
+let observer = new MutationObserver(function(mutations) {
+  carouselPseudoClassHandler();
+
 });
+observer.observe($("#solutionSlider .carousel-inner")[0], {
+  attributes: true,
+  characterData: true,
+  childList: true,
+  subtree: true,
+  attributeOldValue: true,
+  characterDataOldValue: true
+});
+
+
+
 // MODIFY MENU BAR ON SCROLL DOWN
 // $(window).on('load scroll resize', function(){
 //     console.log('Event Fired', $(window).scrollTop());
